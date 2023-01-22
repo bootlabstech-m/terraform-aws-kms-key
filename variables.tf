723dbfc3b18a34ec95c250542d6a8dad4c5d3dc2
Silpashree <silpashree@bootlabstech.com>
2023-01-22 12:45:35 +0530

    initial commit

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "kms_details" {
+  description = "The aurora cluster details"
+  type        = list(any)
+}
+variable "region" {
+  description = "The region for the resource"
+  type        = string
+}
+variable "role_arn" {
+  description = " The ARN of the IAM role"
+  type = string
+}
+variable "role_arn" {
+  description = " The ARN of the IAM role"
+  type = string
+}
